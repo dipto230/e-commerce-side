ef4cf9cf94870e6a2bd526883982466a784f3dba
dipto230 <diptoroy230305@gmail.com>
2025-08-27 04:04:53 +0530

    newsletterbox updated

---
+++ src/components/NewsletterBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const NewsletterBox = () => {
+  return (
+      <div className='text-center'>
+          <p className='text-2xl font-medium  text-gray=800'>Subscribe now & get 20% off</p>
+          <p className='text-gray-300 mt-3'>
+              Join our community and be the first to know about exclusive deals, new arrivals, and more
+          </p>
+          <from className='w-full  sm:w-1/2 flex items-center  gap-3  mx-auto  my-6  border  pl-3 '>
+              <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter Your Email' required />
+              <button type='submit' className='bg-black text-white text-xs px-10 py-4 '>SUBSCRIBE</button>
+          </from>
+    </div>
+  )
+}
+
+export default NewsletterBox
