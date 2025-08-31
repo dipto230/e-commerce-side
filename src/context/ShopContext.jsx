498782399de5222bb0bebe2c13c94b5b42ff1e8d
dipto230 <diptoroy230305@gmail.com>
2025-08-31 13:47:56 +0530

    frontend section done for login and signup

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { toast } from "react-toastify";
 import Product from './../pages/Product';
+import { useNavigate } from "react-router-dom";
 
 
 export const ShopContext = createContext();
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
         if (!size) {
@@ -79,7 +81,7 @@ const ShopContextProvider = (props) => {
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount, updateQuantity,
-        getCartAmount
+        getCartAmount,navigate
         
     }
     return (
